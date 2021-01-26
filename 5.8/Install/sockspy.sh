9320f662a1e0163a1acd1d9a71b757b752096eef
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-01-25 21:58:38 -0500

    Update sockspy.sh

--- 5.8/Install/sockspy.sh
+++ 5.8/Install/sockspy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;31m" [3]="\033[1;33m" [4]="\033[1;32m" )
 SCPfrm="/etc/VpsPackdir" && [[ ! -d ${SCPfrm} ]] && exit
-SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
+SCPinst="/etc/VpsPackdir" && [[ ! -d ${SCPinst} ]] && exit
 meu_ip () {
 if [[ -e /etc/MEUIPADM ]]; then
 echo "$(cat /etc/MEUIPADM)"
